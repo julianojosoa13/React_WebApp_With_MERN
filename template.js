11d9ada29bb561ec30f7302719acc5bde1e09bfd
Juliano_Rahajaharima <josoa.juliano.rahajaharimanga@esti.mg>
2022-07-21 11:36:13 +0300

    Hello World in development server with Express

--- template.js
+++ template.js
@@ -1,5 +1,5 @@
 export default () => {
-    return <!doctype html>
+    return `<!doctype html>
         <html lang= "en">
             <head>
                 <meta charset="utf-8" />
@@ -9,5 +9,5 @@ export default () => {
                 <div id="root"></div>
                 <script type="text/javascript" src="/dist/bundle.js"></script>
             </body>
-        </html>
+        </html>`
 }
